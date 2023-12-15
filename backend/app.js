900be3c82be110dc8f7770737bf572f2e84b65e8
IKala4I <vpersko@gmail.com>
2023-12-15 14:56:37 +0200

    * fix error on backend

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ import express from 'express';
 import indexRouter from './routes/index.js';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:4200']
+    origin: ['http://localhost:4200','https://qa-course-work.onrender.com']
 }));
 
 app.use('/', indexRouter);
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -3,6 +3,6 @@ import {environment} from '../../../environments/environment';
 let BASE_URL = 'http://localhost:5000/statistic';
 
 if (environment.production)
-  BASE_URL = '';
+  BASE_URL = 'https://qa-course-work.onrender.com/statistic';
 
 export {BASE_URL};
